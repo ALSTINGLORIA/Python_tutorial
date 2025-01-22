4b64e1cb5062475a4bc46d448be0041cb63acbc5
alstingloria <alstingloria@gmail.com>
2025-01-22 12:25:11 +0530

    change tax slabs

--- income_tax.py
+++ income_tax.py
@@ -15,6 +15,6 @@ elif(actualSalary > 10_00_000 and actualSalary <= 12_00_000):
 elif(actualSalary >12_00_000 and actualSalary <= 15_00_000):
     tax = tax = ((actualSalary - 12_00_000) * 0.20) + (10_00_000 * 0.15) + (7_00_000 * 0.10) + (3_00_000 * 0.05)
 else:
-    tax = tax = ((actualSalary - 15_00_000) * 0.20) + (12_00_000 * 0.30) + (10_00_000 * 0.15) + (7_00_000 * 0.10) + (3_00_000 * 0.05)
+    tax = tax = ((actualSalary - 15_00_000) * 0.30) + (12_00_000 * 0.20) + (10_00_000 * 0.15) + (7_00_000 * 0.10) + (3_00_000 * 0.05)
 
 print("\n the amount of tax you have to pay is :",tax)
